f4c3f84b9827ed3adbca9290a82c27d988811623
Ahtesham Akram <ahte02254@gmail.com>
2023-05-13 17:03:20 +0500

    class 01

---
+++ class 01/app.js
@@ -0,0 +1,52 @@
+var tech;
+/*
+    CASES :- 
+    1. camelCase
+    2. snake_case
+    3. Pascalcase
+    4. kebab-case
+
+    VARIABLES:- 
+    ES5:
+        var 
+    ES6:- 
+        let 
+        const 
+
+    RULES FOR CREATING AN IDENTIFIER:-
+    1. number 
+*/
+
+// var 
+// var myName = "ahtesham";
+// var myName = "JS"; // redeclaration
+// myName = "javascript" // reinitialization 
+// console.log(myName);
+
+// let
+// let myName = "ahtesham";
+// myName = "JS"  // reintialization
+
+//const 
+// const myName = "ahtesham";
+// myName = "JS"
+// console.log("myName",myName)
+
+/*
+    SCOPE:-
+    1. global scope (var)
+    2. block level scope (let and const)
+
+*/
+
+// let myName; // declare
+// // myName = "jsfs" //initialize 
+// if(true){
+//     myName = "ahtesham";
+// }
+//     // var myName = "ahteshahkadkjsaham";
+// console.log(myName);
+
+// hoisting
+console.log(tech);
+let tech = "js";
